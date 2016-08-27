DirtyUnicorns/android_frameworks_base

Statusbar icons transparency
Change-Id: Ided1030a14effd05394ea05e4293261e7b93b2be

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -388,7 +388,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
     // DU logo
     private boolean mDuLogo;
-    private ImageView duLogo;
+    private ImageView mDuLogoView;
 
     private int mNavigationBarWindowState = WINDOW_STATE_SHOWING;
 
@@ -1237,6 +1237,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 (ViewStub) mStatusBarWindowContent.findViewById(R.id.keyguard_user_switcher),
                 mKeyguardStatusBar, mNotificationPanel, mUserSwitcherController);
 
+        mDuLogoView = (ImageView) mStatusBarView.findViewById(R.id.du_logo);
 
         // Set up the quick settings tile panel
         mQSPanel = (QSPanel) mStatusBarWindowContent.findViewById(R.id.quick_settings_panel);
@@ -1872,6 +1873,14 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
     public static boolean isTopLevelChild(Entry entry) {
         return entry.row.getParent() instanceof NotificationStackScrollLayout;
+    }
+
+    @Override
+    protected void updateStatusBarIconsAlpha(int alpha) {
+        mIconController.updateIconAlpha(alpha);
+        if (mDuLogoView != null) {
+            mDuLogoView.setAlpha(alpha);
+        }
     }
 
     @Override
@@ -3489,12 +3498,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
     };
 
-    public void showDuLogo(boolean show) {
+    private void showDuLogo(boolean show) {
         if (mStatusBarView == null) return;
-        ContentResolver resolver = mContext.getContentResolver();
-        duLogo = (ImageView) mStatusBarView.findViewById(R.id.du_logo);
-        if (duLogo != null) {
-            duLogo.setVisibility(show ? (mDuLogo ? View.VISIBLE : View.GONE) : View.GONE);
+        if (mDuLogoView != null) {
+            mDuLogoView.setVisibility(show ? (mDuLogo ? View.VISIBLE : View.GONE) : View.GONE);
         }
     }
 

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
@@ -27,12 +27,7 @@ import com.android.systemui.statusbar.BarTransitions;
 import com.android.systemui.R;
 
 public final class PhoneStatusBarTransitions extends BarTransitions {
-    private static final float ICON_ALPHA_WHEN_NOT_OPAQUE = 1;
-    private static final float ICON_ALPHA_WHEN_LIGHTS_OUT_BATTERY_CLOCK = 0.5f;
-    private static final float ICON_ALPHA_WHEN_LIGHTS_OUT_NON_BATTERY_CLOCK = 0;
-
     private final PhoneStatusBarView mView;
-    private final float mIconAlphaWhenOpaque;
 
     private View mLeftSide, mStatusIcons, mSignalCluster, mBattery, mClock, mCenterClock, mNetworkTraffic, mDuLogo, mMinitBattery;
 
@@ -44,8 +39,6 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
                 R.color.status_bar_background_transparent,
                 com.android.internal.R.color.battery_saver_mode_color);
         mView = view;
-        final Resources res = mView.getContext().getResources();
-        mIconAlphaWhenOpaque = res.getFraction(R.dimen.status_bar_icon_drawing_alpha, 1, 1);
     }
 
     public void init() {
@@ -66,22 +59,6 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
         return ObjectAnimator.ofFloat(v, "alpha", v.getAlpha(), toAlpha);
     }
 
-    private float getNonBatteryClockAlphaFor(int mode) {
-        return isLightsOut(mode) ? ICON_ALPHA_WHEN_LIGHTS_OUT_NON_BATTERY_CLOCK
-                : !isOpaque(mode) ? ICON_ALPHA_WHEN_NOT_OPAQUE
-                : mIconAlphaWhenOpaque;
-    }
-
-    private float getBatteryClockAlpha(int mode) {
-        return isLightsOut(mode) ? ICON_ALPHA_WHEN_LIGHTS_OUT_BATTERY_CLOCK
-                : getNonBatteryClockAlphaFor(mode);
-    }
-
-    private boolean isOpaque(int mode) {
-        return !(mode == MODE_SEMI_TRANSPARENT || mode == MODE_TRANSLUCENT
-                || mode == MODE_TRANSPARENT || mode == MODE_LIGHTS_OUT_TRANSPARENT);
-    }
-
     @Override
     protected void onTransition(int oldMode, int newMode, boolean animate) {
         super.onTransition(oldMode, newMode, animate);
@@ -90,23 +67,19 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
 
     private void applyMode(int mode, boolean animate) {
         if (mLeftSide == null) return; // pre-init
-        float newAlpha = getNonBatteryClockAlphaFor(mode);
-        float newAlphaBC = getBatteryClockAlpha(mode);
+        float newIconAlpha = mView.getIconAlpha();
         if (mCurrentAnimation != null) {
             mCurrentAnimation.cancel();
         }
         if (animate) {
             AnimatorSet anims = new AnimatorSet();
             anims.playTogether(
-                    animateTransitionTo(mLeftSide, newAlpha),
-                    animateTransitionTo(mStatusIcons, newAlpha),
-                    animateTransitionTo(mSignalCluster, newAlpha),
-                    animateTransitionTo(mNetworkTraffic, newAlpha),
-                    animateTransitionTo(mBattery, newAlphaBC),
-                    animateTransitionTo(mClock, newAlphaBC),
-                    animateTransitionTo(mCenterClock, newAlphaBC),
-                    animateTransitionTo(mDuLogo, newAlphaBC),
-                    animateTransitionTo(mMinitBattery, newAlphaBC)
+                    animateTransitionTo(mSignalCluster, newIconAlpha),
+                    animateTransitionTo(mNetworkTraffic, newIconAlpha),
+                    animateTransitionTo(mBattery, newIconAlpha),
+                    animateTransitionTo(mClock, newIconAlpha),
+                    animateTransitionTo(mCenterClock, newIconAlpha),
+                    animateTransitionTo(mMinitBattery, newIconAlpha)
                     );
             if (isLightsOut(mode)) {
                 anims.setDuration(LIGHTS_OUT_DURATION);
@@ -114,15 +87,12 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
             anims.start();
             mCurrentAnimation = anims;
         } else {
-            mLeftSide.setAlpha(newAlpha);
-            mStatusIcons.setAlpha(newAlpha);
-            mSignalCluster.setAlpha(newAlpha);
-            mNetworkTraffic.setAlpha(newAlpha);
-            mBattery.setAlpha(newAlphaBC);
-            mClock.setAlpha(newAlphaBC);
-            mCenterClock.setAlpha(newAlphaBC);
-            mDuLogo.setAlpha(newAlphaBC);
-            mMinitBattery.setAlpha(newAlphaBC);
+            mSignalCluster.setAlpha(newIconAlpha);
+            mNetworkTraffic.setAlpha(newIconAlpha);
+            mBattery.setAlpha(newIconAlpha);
+            mClock.setAlpha(newIconAlpha);
+            mCenterClock.setAlpha(newIconAlpha);
+            mMinitBattery.setAlpha(newIconAlpha);
         }
     }
 }

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
@@ -56,6 +56,7 @@ public class PhoneStatusBarView extends PanelBar {
         }
     };
 
+    private float mCarrierLabelAlpha;
     private int mShowCarrierLabel;
     private TextView mCarrierLabel;
 
@@ -89,6 +90,17 @@ public class PhoneStatusBarView extends PanelBar {
     private void showStatusBarCarrier() {
         mShowCarrierLabel = Settings.System.getIntForUser(getContext().getContentResolver(),
                 Settings.System.STATUS_BAR_SHOW_CARRIER, 1, UserHandle.USER_CURRENT);
+        mCarrierLabelAlpha = alphaIntToFloat(Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.STATUS_BAR_ICON_ALPHA, 255, UserHandle.USER_CURRENT));
+    }
+
+    private static float alphaIntToFloat(int alpha) {
+        return (float) Math.max(0, Math.min(255, alpha)) / 255;
+    }
+
+    public float getIconAlpha() {
+        return alphaIntToFloat(Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.STATUS_BAR_ICON_ALPHA, 255, UserHandle.USER_CURRENT));
     }
 
     @Override
@@ -102,8 +114,10 @@ public class PhoneStatusBarView extends PanelBar {
         if (mCarrierLabel != null) {
             if (mShowCarrierLabel == 2) {
                 mCarrierLabel.setVisibility(View.VISIBLE);
+                mCarrierLabel.setAlpha(mCarrierLabelAlpha);
             } else if (mShowCarrierLabel == 3) {
                 mCarrierLabel.setVisibility(View.VISIBLE);
+                mCarrierLabel.setAlpha(mCarrierLabelAlpha);
             } else {
                 mCarrierLabel.setVisibility(View.GONE);
             }
@@ -243,6 +257,8 @@ public class PhoneStatusBarView extends PanelBar {
         super.onAttachedToWindow();
         getContext().getContentResolver().registerContentObserver(Settings.System.getUriFor(
                 "status_bar_show_carrier"), false, mObserver);
+        getContext().getContentResolver().registerContentObserver(Settings.System.getUriFor(
+                "sb_notif_icon_alpha"), false, mObserver);
     }
 
     @Override

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
@@ -457,6 +457,7 @@ public class StatusBarIconController implements Tunable {
         mCarrier.setTextColor(mIconTint);
         mNetworkTraffic.setDarkIntensity(mDarkIntensity);
         applyNotificationIconsTint();
+        applyIconAlpha();
     }
 
     private void applyNotificationIconsTint() {
@@ -468,6 +469,54 @@ public class StatusBarIconController implements Tunable {
                 v.setImageTintList(ColorStateList.valueOf(mIconTint));
             }
         }
+    }
+
+    public void applyIconAlpha() {
+        final float iconAlpha = setIconAlpha();
+        mStatusIcons.setAlpha(iconAlpha);
+        mSignalCluster.setAlpha(iconAlpha);
+        mMoreIcon.setAlpha(iconAlpha);
+        mDuLogo.setAlpha(iconAlpha);
+        mBatteryLevelTextView.setAlpha(iconAlpha);
+        mBatteryMeterView.setAlpha(iconAlpha);
+        mClock.setAlpha(iconAlpha);
+        mCclock.setAlpha(iconAlpha);
+        mLeftClock.setAlpha(iconAlpha);
+        mCarrier.setAlpha(iconAlpha);
+        mNetworkTraffic.setAlpha(iconAlpha);
+    }
+
+    public void updateIconAlpha(int alpha) {
+        final float newAlpha = alphaIntToFloat(alpha);
+        for (int i = 0; i < mNotificationIcons.getChildCount(); i++) {
+            StatusBarIconView v = (StatusBarIconView) mNotificationIcons.getChildAt(i);
+            boolean isPreL = Boolean.TRUE.equals(v.getTag(R.id.icon_is_pre_L));
+            boolean colorize = !isPreL || isGrayscale(v);
+            if (colorize) {
+                v.setAlpha(alpha);
+            }
+        }
+        mStatusIcons.setAlpha(newAlpha);
+        mSignalCluster.setAlpha(newAlpha);
+        mMoreIcon.setAlpha(newAlpha);
+        mDuLogo.setAlpha(newAlpha);
+        mBatteryLevelTextView.setAlpha(newAlpha);
+        mBatteryMeterView.setAlpha(newAlpha);
+        mClock.setAlpha(newAlpha);
+        mCclock.setAlpha(newAlpha);
+        mLeftClock.setAlpha(newAlpha);
+        mCarrier.setAlpha(newAlpha);
+        mNetworkTraffic.setAlpha(newAlpha);
+    }
+
+    private float setIconAlpha() {
+        return alphaIntToFloat(Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_ICON_ALPHA, 255,
+                UserHandle.USER_CURRENT));
+    }
+
+    private static float alphaIntToFloat(int alpha) {
+        return (float) Math.max(0, Math.min(255, alpha)) / 255;
     }
 
     private boolean isGrayscale(StatusBarIconView v) {

Suicide-Squad-Rom/android_frameworks_base2

Signed-off-by: Varun Date <date.varun123@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -2862,6 +2862,16 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         return entry.row.getParent() instanceof NotificationStackScrollLayout;
     }
 
+    protected void updateStatusBarIconsAlpha(int alpha) {
+        mIconController.updateIconAlpha(alpha);
+        if (rrLogo != null) {
+            rrLogo.setAlpha(alpha);
+        }
+	if (mCLogo != null) {
+	    mCLogo.setAlpha(alpha);
+	}
+    }
+
     @Override
     public void updateNotifications() {
         mNotificationData.filterAndSort();
@@ -4688,10 +4698,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     public void showRRLogo(boolean show , int color , int style) {
         if (mStatusBarView == null) return;
 
-  	 if (!show) {
+ 	if (!show) {
             rrLogo.setVisibility(View.GONE);
             return;
-        }
+	}
 	if (color != 0xFFFFFFFF) {
        	    rrLogo.setColorFilter(color, Mode.SRC_IN);
 	} else {
@@ -4717,10 +4727,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
 	if (mStatusBarView == null) return;
 
-  	 if (!show) {
+ 	if (!show) {
             mCLogo.setVisibility(View.GONE);
             return;
-        }
+	}
 	if (color != 0xFFFFFFFF) {
 		mCLogo.setColorFilter(color, Mode.MULTIPLY);
 	} else {

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
@@ -27,14 +27,9 @@ import com.android.systemui.statusbar.BarTransitions;
 import com.android.systemui.R;
 
 public final class PhoneStatusBarTransitions extends BarTransitions {
-    private static final float ICON_ALPHA_WHEN_NOT_OPAQUE = 1;
-    private static final float ICON_ALPHA_WHEN_LIGHTS_OUT_BATTERY_CLOCK = 0.5f;
-    private static final float ICON_ALPHA_WHEN_LIGHTS_OUT_NON_BATTERY_CLOCK = 0;
-
     private final PhoneStatusBarView mView;
-    private final float mIconAlphaWhenOpaque;
 
-    private View mLeftSide, mStatusIcons, mSignalCluster, mBattery, mClock,mNetworkTraffic, mRRLogo, mWeatherTextView ,mLeftWeatherTextView ,mCLogo, mMinitBattery;
+    private View mLeftSide, mStatusIcons, mSignalCluster, mBattery, mClock, mCenterClock ,mNetworkTraffic, mRRLogo, mWeatherTextView ,mLeftWeatherTextView ,mCLogo, mMinitBattery;
 
     private Animator mCurrentAnimation;
 
@@ -44,8 +39,6 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
                 R.color.status_bar_background_transparent,
                 com.android.internal.R.color.battery_saver_mode_color);
         mView = view;
-        final Resources res = mView.getContext().getResources();
-        mIconAlphaWhenOpaque = res.getFraction(R.dimen.status_bar_icon_drawing_alpha, 1, 1);
     }
 
     public void init() {
@@ -56,7 +49,8 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
         mClock = mView.findViewById(R.id.clock);
         mNetworkTraffic = mView.findViewById(R.id.networkTraffic);
         mRRLogo = mView.findViewById(R.id.rr_logo);
-	mCLogo =mView.findViewById(R.id.custom);
+	mCLogo = mView.findViewById(R.id.custom);
+	mCenterClock = mView.findViewById(R.id.center_clock);
         mWeatherTextView = mView.findViewById(R.id.weather_temp);
         mLeftWeatherTextView = mView.findViewById(R.id.left_weather_temp);
         mMinitBattery = mView.findViewById(R.id.minitBattery);
@@ -68,22 +62,6 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
         return ObjectAnimator.ofFloat(v, "alpha", v.getAlpha(), toAlpha);
     }
 
-    private float getNonBatteryClockAlphaFor(int mode) {
-        return isLightsOut(mode) ? ICON_ALPHA_WHEN_LIGHTS_OUT_NON_BATTERY_CLOCK
-                : !isOpaque(mode) ? ICON_ALPHA_WHEN_NOT_OPAQUE
-                : mIconAlphaWhenOpaque;
-    }
-
-    private float getBatteryClockAlpha(int mode) {
-        return isLightsOut(mode) ? ICON_ALPHA_WHEN_LIGHTS_OUT_BATTERY_CLOCK
-                : getNonBatteryClockAlphaFor(mode);
-    }
-
-    private boolean isOpaque(int mode) {
-        return !(mode == MODE_SEMI_TRANSPARENT || mode == MODE_TRANSLUCENT
-                || mode == MODE_TRANSPARENT || mode == MODE_LIGHTS_OUT_TRANSPARENT);
-    }
-
     @Override
     protected void onTransition(int oldMode, int newMode, boolean animate) {
         super.onTransition(oldMode, newMode, animate);
@@ -92,25 +70,25 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
 
     private void applyMode(int mode, boolean animate) {
         if (mLeftSide == null) return; // pre-init
-        float newAlpha = getNonBatteryClockAlphaFor(mode);
-        float newAlphaBC = getBatteryClockAlpha(mode);
+        float newIconAlpha = mView.getIconAlpha();
         if (mCurrentAnimation != null) {
             mCurrentAnimation.cancel();
         }
         if (animate) {
             AnimatorSet anims = new AnimatorSet();
             anims.playTogether(
-                    animateTransitionTo(mLeftSide, newAlpha),
-                    animateTransitionTo(mStatusIcons, newAlpha),
-                    animateTransitionTo(mSignalCluster, newAlpha),
-                    animateTransitionTo(mNetworkTraffic, newAlpha),
-                    animateTransitionTo(mWeatherTextView, newAlpha),
-                    animateTransitionTo(mLeftWeatherTextView, newAlpha),
-                    animateTransitionTo(mBattery, newAlphaBC),
-                    animateTransitionTo(mClock, newAlphaBC),
-                    animateTransitionTo(mRRLogo, newAlphaBC),
-		            animateTransitionTo(mCLogo, newAlphaBC),
-                    animateTransitionTo(mMinitBattery, newAlphaBC)
+                    animateTransitionTo(mLeftSide, newIconAlpha),
+                    animateTransitionTo(mStatusIcons, newIconAlpha),
+                    animateTransitionTo(mWeatherTextView, newIconAlpha),
+                    animateTransitionTo(mLeftWeatherTextView, newIconAlpha),
+                    animateTransitionTo(mSignalCluster, newIconAlpha),
+                    animateTransitionTo(mNetworkTraffic, newIconAlpha),
+                    animateTransitionTo(mBattery, newIconAlpha),
+                    animateTransitionTo(mClock, newIconAlpha),
+                    animateTransitionTo(mCenterClock, newIconAlpha),
+                    animateTransitionTo(mMinitBattery, newIconAlpha),
+		    animateTransitionTo(mRRLogo, newIconAlpha),
+		    animateTransitionTo(mCLogo, newIconAlpha)
                     );
             if (isLightsOut(mode)) {
                 anims.setDuration(LIGHTS_OUT_DURATION);
@@ -118,17 +96,17 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
             anims.start();
             mCurrentAnimation = anims;
         } else {
-            mLeftSide.setAlpha(newAlpha);
-            mStatusIcons.setAlpha(newAlpha);
-            mSignalCluster.setAlpha(newAlpha);
-            mNetworkTraffic.setAlpha(newAlpha);
-            mWeatherTextView.setAlpha(newAlpha);
-            mLeftWeatherTextView.setAlpha(newAlpha);
-            mBattery.setAlpha(newAlphaBC);
-            mClock.setAlpha(newAlphaBC);
-            mRRLogo.setAlpha(newAlphaBC);
-	        mCLogo.setAlpha(newAlphaBC);
-            mMinitBattery.setAlpha(newAlphaBC);
+            mLeftSide.setAlpha(newIconAlpha);
+            mStatusIcons.setAlpha(newIconAlpha);
+            mSignalCluster.setAlpha(newIconAlpha);
+            mNetworkTraffic.setAlpha(newIconAlpha);
+            mWeatherTextView.setAlpha(newIconAlpha);
+            mLeftWeatherTextView.setAlpha(newIconAlpha);
+            mBattery.setAlpha(newIconAlpha);
+            mClock.setAlpha(newIconAlpha);
+            mRRLogo.setAlpha(newIconAlpha);
+	    mCLogo.setAlpha(newIconAlpha);
+            mMinitBattery.setAlpha(newIconAlpha);
         }
     }
 }

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
@@ -58,6 +58,7 @@ public class PhoneStatusBarView extends PanelBar {
         }
     };
 
+    private float mCarrierLabelAlpha;
     private int mShowCarrierLabel;
     private TextView mCarrierLabel;
 
@@ -122,6 +123,17 @@ public class PhoneStatusBarView extends PanelBar {
         mCarrierLabelFontStyle = Settings.System.getIntForUser(resolver,
                 Settings.System.STATUS_BAR_CARRIER_FONT_STYLE, FONT_NORMAL,
                 UserHandle.USER_CURRENT);
+        mCarrierLabelAlpha = alphaIntToFloat(Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.STATUS_BAR_ICON_ALPHA, 255, UserHandle.USER_CURRENT));
+    }
+
+    private static float alphaIntToFloat(int alpha) {
+        return (float) Math.max(0, Math.min(255, alpha)) / 255;
+    }
+
+    public float getIconAlpha() {
+        return alphaIntToFloat(Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.STATUS_BAR_ICON_ALPHA, 255, UserHandle.USER_CURRENT));
     }
 
     @Override
@@ -135,8 +147,10 @@ public class PhoneStatusBarView extends PanelBar {
         if (mCarrierLabel != null) {
             if (mShowCarrierLabel == 2) {
                 mCarrierLabel.setVisibility(View.VISIBLE);
+                mCarrierLabel.setAlpha(mCarrierLabelAlpha);
             } else if (mShowCarrierLabel == 3) {
                 mCarrierLabel.setVisibility(View.VISIBLE);
+                mCarrierLabel.setAlpha(mCarrierLabelAlpha);
             } else {
                 mCarrierLabel.setVisibility(View.GONE);
 
@@ -389,7 +403,8 @@ public class PhoneStatusBarView extends PanelBar {
                 "status_bar_show_carrier"), false, mObserver);
         getContext().getContentResolver().registerContentObserver(Settings.System.getUriFor(
                 "status_bar_carrier_font_style"), false, mObserver);
-
+        getContext().getContentResolver().registerContentObserver(Settings.System.getUriFor(
+                "status_bar_icon_alpha"), false, mObserver);
     }
 
     @Override

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
@@ -148,6 +148,10 @@ public class StatusBarIconController implements Tunable {
     private ImageView mCLogo;
     private NetworkTraffic mNetworkTraffic;
 
+    private Clock mClock;
+    private Clock mCclock;
+    private Clock mLeftClock;
+
     private final ArraySet<String> mIconBlacklist = new ArraySet<>();
 
     private final Runnable mTransitionDeferringDoneRunnable = new Runnable() {
@@ -185,6 +189,9 @@ public class StatusBarIconController implements Tunable {
 	mRRLogoRight = (ImageView) statusBar.findViewById(R.id.rr_logo);
 	mRRLogoCenter = (ImageView) statusBar.findViewById(R.id.center_rr_logo);
 	mRRLogoBefore = (ImageView) statusBar.findViewById(R.id.before_icons_rr_logo);
+	mClock = (Clock) statusBar.findViewById(R.id.clock);
+	mCclock = (Clock) statusBar.findViewById(R.id.center_clock);
+	mLeftClock = (Clock) statusBar.findViewById(R.id.left_clock);
         mCarrierLabel = (TextView) statusBar.findViewById(R.id.statusbar_carrier_text);
         mWeather = (TextView) statusBar.findViewById(R.id.weather_temp);
         mWeatherLeft = (TextView) statusBar.findViewById(R.id.left_weather_temp);
@@ -252,7 +259,7 @@ public class StatusBarIconController implements Tunable {
                 com.android.internal.R.dimen.status_bar_icon_size);
         mIconHPadding = mContext.getResources().getDimensionPixelSize(
                 R.dimen.status_bar_icon_padding);
-        //mClockController.updateFontSize();
+        FontSizeUtils.updateFontSize(mCarrier, R.dimen.carrier_label_height);
         carrierLabelVisibility();
     }
 
@@ -636,6 +643,7 @@ public class StatusBarIconController implements Tunable {
  	mNetworkTraffic.setDarkIntensity(mDarkIntensity);
 	}
         applyNotificationIconsTint();
+        applyIconAlpha();
     }
 
     private void applyNotificationIconsTint() {
@@ -653,6 +661,60 @@ public class StatusBarIconController implements Tunable {
 		}
             }
         }
+    }
+
+    public void applyIconAlpha() {
+        final float iconAlpha = setIconAlpha();
+	try {
+        mStatusIcons.setAlpha(iconAlpha);
+        mSignalCluster.setAlpha(iconAlpha);
+        mMoreIcon.setAlpha(iconAlpha);
+        mRRLogo.setAlpha(iconAlpha);
+	mCLogo.setAlpha(iconAlpha);
+        mBatteryLevelTextView.setAlpha(iconAlpha);
+        mBatteryMeterView.setAlpha(iconAlpha);
+        mClock.setAlpha(iconAlpha);
+        mCclock.setAlpha(iconAlpha);
+        mLeftClock.setAlpha(iconAlpha);
+        mCarrierLabel.setAlpha(iconAlpha);
+        mNetworkTraffic.setAlpha(iconAlpha);
+	} catch (Exception e) {}
+    }
+
+    public void updateIconAlpha(int alpha) {
+        final float newAlpha = alphaIntToFloat(alpha);
+        for (int i = 0; i < mNotificationIcons.getChildCount(); i++) {
+            StatusBarIconView v = (StatusBarIconView) mNotificationIcons.getChildAt(i);
+            boolean isPreL = Boolean.TRUE.equals(v.getTag(R.id.icon_is_pre_L));
+            boolean colorize = !isPreL || isGrayscale(v);
+            if (colorize) {
+                v.setAlpha(alpha);
+            }
+        }
+	try {
+        mStatusIcons.setAlpha(newAlpha);
+        mSignalCluster.setAlpha(newAlpha);
+        mMoreIcon.setAlpha(newAlpha);
+	mCLogo.setAlpha(newAlpha);
+        mRRLogo.setAlpha(newAlpha);
+        mBatteryLevelTextView.setAlpha(newAlpha);
+        mBatteryMeterView.setAlpha(newAlpha);
+        mClock.setAlpha(newAlpha);
+        mCclock.setAlpha(newAlpha);
+        mLeftClock.setAlpha(newAlpha);
+        mCarrierLabel.setAlpha(newAlpha);
+        mNetworkTraffic.setAlpha(newAlpha);
+	} catch (Exception e) {}
+    }
+
+    private float setIconAlpha() {
+        return alphaIntToFloat(Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_ICON_ALPHA, 255,
+                UserHandle.USER_CURRENT));
+    }
+
+    private static float alphaIntToFloat(int alpha) {
+        return (float) Math.max(0, Math.min(255, alpha)) / 255;
     }
 
     private boolean isGrayscale(StatusBarIconView v) {
@@ -983,6 +1045,9 @@ public class StatusBarIconController implements Tunable {
          ContentResolver resolver = mContext.getContentResolver();
          resolver.registerContentObserver(Settings.System
                  .getUriFor(Settings.System.HIDE_CARRIER_MAX_SWITCH),
+                 false, this, UserHandle.USER_CURRENT);
+         resolver.registerContentObserver(Settings.System
+                 .getUriFor(Settings.System.HIDE_CARRIER_MAX_NOTIFICATION),
                  false, this, UserHandle.USER_CURRENT);
          resolver.registerContentObserver(Settings.System
                  .getUriFor(Settings.System.HIDE_CARRIER_MAX_NOTIFICATION),